dc3dcfa6279f75d8e47adaa3856e7bb932ba423b
Shay Baz <shay@rounds.com>
2019-04-14 19:59:57 +0300

    modeldata

--- movekinlib/src/main/java/org/kinecosystem/movekinlib/discovery/view/DiscoverButton.kt
+++ movekinlib/src/main/java/org/kinecosystem/movekinlib/discovery/view/DiscoverButton.kt
@@ -1,8 +1,8 @@
 package org.kinecosystem.movekinlib.discovery.view
 
 import android.content.Context
-import android.os.Build
 import android.support.v4.content.ContextCompat
+import android.support.v4.widget.TextViewCompat
 import android.util.AttributeSet
 import android.widget.TextView
 import org.kinecosystem.movekinlib.R
@@ -10,7 +10,7 @@ import org.kinecosystem.movekinlib.discovery.presenter.DiscoverButtonPresenter
 import org.kinecosystem.movekinlib.discovery.presenter.IDiscoverButtonPresenter
 
 class DiscoverButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-    TextView(context, attrs, defStyleAttr), IDiscoverButtonView {
+        TextView(context, attrs, defStyleAttr), IDiscoverButtonView {
 
     override fun startAppsDiscoveryActivity() {
         context.startActivity(AppsDiscoveryActivity.getIntent(context))
@@ -20,11 +20,7 @@ class DiscoverButton @JvmOverloads constructor(context: Context, attrs: Attribut
 
     init {
         text = resources.getString(R.string.discover)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            setTextAppearance(R.style.kinTextButtonRounded)
-        } else {
-            setTextAppearance(context, R.style.kinTextButtonRounded)
-        }
+        TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded)
         background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable)
         presenter = DiscoverButtonPresenter()
         setOnClickListener {
