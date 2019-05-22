050facd9f92fa2a0fc7bac2be08e000631c595f3
Shay Baz <shay@rounds.com>
2019-05-22 16:27:30 +0300

    KINNO-499-add_buttons

--- appsDiscovery/src/main/java/org/kinecosystem/appsdiscovery/sender/discovery/view/customView/AppsDiscoveryButton.kt
+++ appsDiscovery/src/main/java/org/kinecosystem/appsdiscovery/sender/discovery/view/customView/AppsDiscoveryButton.kt
@@ -7,6 +7,10 @@ import android.util.AttributeSet
 import android.widget.TextView
 import org.kinecosystem.appsdiscovery.R
 import org.kinecosystem.appsdiscovery.sender.discovery.view.AppsDiscoveryActivity
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.graphics.drawable.Drawable
+
 
 class AppsDiscoveryButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
         TextView(context, attrs, defStyleAttr) {
@@ -15,12 +19,79 @@ class AppsDiscoveryButton @JvmOverloads constructor(context: Context, attrs: Att
         context.startActivity(AppsDiscoveryActivity.getIntent(context))
     }
 
+    companion object{
+        enum class ButtonTheme{
+            DARK,
+            LIGHT
+        }
+    }
+
+    var hasIcon: Boolean = false
+    var theme:ButtonTheme = Companion.ButtonTheme.DARK
+    private var isEnabledInternal = isEnabled
+    private val drawable: Drawable? = ContextCompat.getDrawable(context, R.drawable.explorekin_icon_white)
+
     init {
+        attrs?.let {
+            val typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.AppsDiscoveryButton)
+            hasIcon = typedArray.getBoolean(R.styleable.AppsDiscoveryButton_has_drawable, false)
+            theme = ButtonTheme.values()[typedArray.getInt(R.styleable.AppsDiscoveryButton_button_theme, 0)]
+            typedArray.recycle()
+        }
         text = resources.getString(R.string.explore_kin)
-        TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded_Purple)
-        background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable)
+        if(theme == Companion.ButtonTheme.DARK){
+            TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded_Purple)
+            background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable)
+        }else if(theme == Companion.ButtonTheme.LIGHT){
+            TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded_White)
+            background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable_light)
+        }
+        compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.button_drawable_padding)
         setOnClickListener {
             startAppsDiscoveryActivity()
         }
+        onEnabledChanged()
+
+        viewTreeObserver.addOnDrawListener {
+            if (isEnabledInternal != isEnabled) {
+                isEnabledInternal = isEnabled
+                onEnabledChanged()
+            }
+        }
     }
+
+    fun showIcon(hasIcon:Boolean){
+        this.hasIcon = hasIcon
+    }
+
+    private fun onEnabledChanged() {
+        updateView()
+    }
+
+    private fun updateView(){
+        updateIconColor()
+        invalidate()
+    }
+
+    private fun updateIconColor() {
+        if (hasIcon) {
+            val normalColorRes = if(theme == Companion.ButtonTheme.DARK) R.color.white else R.color.purple
+            val res = if (isEnabled) normalColorRes else R.color.grey_dark
+            val color = ContextCompat.getColor(context, res)
+
+            if(normalColorRes == R.color.white){
+                val currentDrawable: Drawable? = drawable?.mutate()
+                currentDrawable?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SCREEN)
+                setCompoundDrawablesWithIntrinsicBounds(currentDrawable, null, null, null)
+
+            }else {
+                val currentDrawable: Drawable? = drawable?.mutate()
+                currentDrawable?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY)
+                setCompoundDrawablesWithIntrinsicBounds(currentDrawable, null, null, null)
+            }
+        }else{
+            setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
+        }
+    }
+
 }
--- senderSampleApp/src/main/java/org/kinecosystem/sampleSenderApp/SenderMainActivity.java
+++ senderSampleApp/src/main/java/org/kinecosystem/sampleSenderApp/SenderMainActivity.java
@@ -2,8 +2,10 @@ package org.kinecosystem.sampleSenderApp;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 import org.kinecosystem.appsdiscovery.sender.discovery.view.customView.AppsDiscoveryAlertDialog;
+import org.kinecosystem.appsdiscovery.sender.discovery.view.customView.AppsDiscoveryButton;
 import org.kinecosystem.baseSampleApp.SampleBaseActivity;
 
 public class SenderMainActivity extends SampleBaseActivity {
@@ -16,9 +18,16 @@ public class SenderMainActivity extends SampleBaseActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final AppsDiscoveryButton btn1 = findViewById(R.id.discoverBtnDark);
+        final AppsDiscoveryButton btn2 = findViewById(R.id.discoverBtnLight);
+
         findViewById(R.id.discoverAlert).setOnClickListener(v -> {
-            AppsDiscoveryAlertDialog dialog = new AppsDiscoveryAlertDialog(SenderMainActivity.this);
-            dialog.show();
+            btn1.setEnabled(!btn1.isEnabled());
+            btn2.setEnabled(!btn2.isEnabled());
+
+            // btn.showIcon(btn.isEnabled());
+           // AppsDiscoveryAlertDialog dialog = new AppsDiscoveryAlertDialog(SenderMainActivity.this);
+           // dialog.show();
         });
     }
 }
