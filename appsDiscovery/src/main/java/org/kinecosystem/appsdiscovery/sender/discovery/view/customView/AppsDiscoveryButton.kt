e4f74f600a796a96fb647580872fe3dd8e7a2987
Shay Baz <shay@rounds.com>
2019-05-26 13:31:26 +0300

    add_button_theme

--- appsDiscovery/src/main/java/org/kinecosystem/appsdiscovery/sender/discovery/view/customView/AppsDiscoveryButton.kt
+++ appsDiscovery/src/main/java/org/kinecosystem/appsdiscovery/sender/discovery/view/customView/AppsDiscoveryButton.kt
@@ -26,15 +26,11 @@ class AppsDiscoveryButton @JvmOverloads constructor(context: Context, attrs: Att
         }
     }
 
-    var hasIcon: Boolean = false
     var theme:ButtonTheme = Companion.ButtonTheme.DARK
-    private var isEnabledInternal = isEnabled
-    private val drawable: Drawable? = ContextCompat.getDrawable(context, R.drawable.explorekin_icon_white)
 
     init {
         attrs?.let {
             val typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.AppsDiscoveryButton)
-            hasIcon = typedArray.getBoolean(R.styleable.AppsDiscoveryButton_has_drawable, false)
             theme = ButtonTheme.values()[typedArray.getInt(R.styleable.AppsDiscoveryButton_button_theme, 0)]
             typedArray.recycle()
         }
@@ -46,52 +42,8 @@ class AppsDiscoveryButton @JvmOverloads constructor(context: Context, attrs: Att
             TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded_White)
             background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable_light)
         }
-        compoundDrawablePadding = resources.getDimensionPixelSize(R.dimen.button_drawable_padding)
         setOnClickListener {
             startAppsDiscoveryActivity()
         }
-        onEnabledChanged()
-
-        viewTreeObserver.addOnDrawListener {
-            if (isEnabledInternal != isEnabled) {
-                isEnabledInternal = isEnabled
-                onEnabledChanged()
-            }
-        }
     }
-
-    fun showIcon(hasIcon:Boolean){
-        this.hasIcon = hasIcon
-    }
-
-    private fun onEnabledChanged() {
-        updateView()
-    }
-
-    private fun updateView(){
-        updateIconColor()
-        invalidate()
-    }
-
-    private fun updateIconColor() {
-        if (hasIcon) {
-            val normalColorRes = if(theme == Companion.ButtonTheme.DARK) R.color.white else R.color.purple
-            val res = if (isEnabled) normalColorRes else R.color.grey_dark
-            val color = ContextCompat.getColor(context, res)
-
-            if(normalColorRes == R.color.white){
-                val currentDrawable: Drawable? = drawable?.mutate()
-                currentDrawable?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SCREEN)
-                setCompoundDrawablesWithIntrinsicBounds(currentDrawable, null, null, null)
-
-            }else {
-                val currentDrawable: Drawable? = drawable?.mutate()
-                currentDrawable?.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.MULTIPLY)
-                setCompoundDrawablesWithIntrinsicBounds(currentDrawable, null, null, null)
-            }
-        }else{
-            setCompoundDrawablesWithIntrinsicBounds(null, null, null, null)
-        }
-    }
-
 }
