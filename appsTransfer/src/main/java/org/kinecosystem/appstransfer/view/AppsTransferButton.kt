0ca3f49eaee7b870e3ef110828f226767646acc8
Shay Baz <shay@rounds.com>
2019-05-27 15:26:21 +0300

    module_lean first init

---
+++ appsTransfer/src/main/java/org/kinecosystem/appstransfer/view/AppsTransferButton.kt
@@ -0,0 +1,27 @@
+package org.kinecosystem.appstransfer.view
+
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v4.widget.TextViewCompat
+import android.util.AttributeSet
+import android.util.Log
+import android.widget.TextView
+import org.kinecosystem.appstransfer.R
+
+class AppsTransferButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        TextView(context, attrs, defStyleAttr) {
+
+    private fun startAppsDiscoveryActivity() {
+       // context.startActivity(AppsDiscoveryActivity.getIntent(context))
+        Log.d("", "#### startAppsTransferActivity")
+    }
+
+    init {
+        text = resources.getString(R.string.transfer_kin)
+        TextViewCompat.setTextAppearance(this, R.style.kinTextButtonRounded_Purple)
+        background = ContextCompat.getDrawable(context, R.drawable.kin_button_rounded_drawable)
+        setOnClickListener {
+            startAppsDiscoveryActivity()
+        }
+    }
+}
