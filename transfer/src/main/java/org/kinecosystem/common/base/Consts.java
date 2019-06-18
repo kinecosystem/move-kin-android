1409d227131e280d86b3b28a5f0760aefafbe2a0
Shay Baz <shay@rounds.com>
2019-06-18 14:55:58 +0300

    testVersion

--- transfer/src/main/java/org/kinecosystem/common/base/Consts.java
+++ transfer/src/main/java/org/kinecosystem/common/base/Consts.java
@@ -6,6 +6,4 @@ public class Consts {
     public static final String RECEIVER_SERVICE_NAME = "ReceiveKinService";
     public static final int NO_BALANCE = -1;
     public static final long TRANSACTION_TIMEOUT = 30 * 1000L;
-
-
 }
