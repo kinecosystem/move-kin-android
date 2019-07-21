ecd4b777bbd400e550ac5f59f1f0e62e4bd42d3e
Shay Baz <shay@rounds.com>
2019-07-21 13:09:18 +0300

    test timeout

--- transfer/src/main/java/org/kinecosystem/common/base/Consts.java
+++ transfer/src/main/java/org/kinecosystem/common/base/Consts.java
@@ -5,7 +5,7 @@ public class Consts {
     public static final String SENDER_SERVICE_NAME = "SendKinService";
     public static final String RECEIVER_SERVICE_NAME = "ReceiveKinService";
     public static final int NO_BALANCE = -1;
-    public static final long TRANSACTION_TIMEOUT = 15 * 1000L;
+    public static final long TRANSACTION_TIMEOUT = 2 * 1000L;
     public static final int MEMO_BYTES_LENGTH_LIMIT = 21;
     public static final String APPS_TRANSFER_MEMO_PREFIX = "xapp";
     public static final String MEMO_UNDERSCORE_DELIMITER = "_";
